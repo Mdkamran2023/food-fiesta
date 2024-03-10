76598c379494e0331c28b202e64058c275cf99b2
Kamran <mdkamran0109@gmail.com>
2024-03-11 01:32:46 +0530

    firebase deploy

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,7 +5,8 @@
 // default export
 export default CDN_URL;
 
- const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId="
+//  const MENU_API = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9715987&lng=77.5945627&restaurantId="
+const MENU_API="https://cors-proxy-unjt.onrender.com/restaurants/";
 
  const SWIGGY_RESTAURANT_CARDS_API="https://cors-proxy-unjt.onrender.com/restaurant-cards"
 
--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -5,11 +5,15 @@ const useRestaurantMenu = (resId) => {
   // fetch data
   const [resInfo, setResInfo] = useState(null);
 
+  console.log("use restaurant called")
   useEffect(() => {
     fetchData();
   }, []);
 
   const fetchData = async () => {
+    let url = MENU_API + resId
+    console.log("url: ", url);
+
    const data = await fetch(MENU_API + resId) ;
     const json = await data.json();
     setResInfo(json.data);
